30ac55af049b955a99ab852de5f49a3bdfe93f90
Alex Monso <amonsosanz@gmail.com>
2023-11-09 11:11:45 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # zkid-labs-update-ecosystem-logos
+
 A node script to update the logos from the ecosystem partners
+
+# script usage
+
+ * Copy the organizations response from the API. Using Chrome dev tools, right click the "Preview" of the response and select "Store object as global variable"
+ * Using Chrome dev tools console, execute this command: `copy(temp1.map(({name, id}) => ({name, id})).sort((a, b) => a.name.localeCompare(b.name)));`
+ * Paste the content in the clipboard in `logo_ids.json`
+ * Make sure you updated `SERVER_URL` and `TOKEN` constants in `push-logos.js` and run the script to update the logos with `node push-logos.js`
+
+# logo manipulation
+
+All the logos are stored in the GIMP file `all-logos-gimp-project.xcf`. Add new logos as required and use the plugin [Export Layers](https://kamilburda.github.io/gimp-export-layers/) to exporting them in a batch.
+
+Some notes:
+
+ * The name of the layer in GIMP must match the name of the organization in `logo_ids.json` but **removing all the dots (.)**.
+ * When exporting, make sure the option "Use layer size" is selected in the plugin, so all exported PNGs are 400x400.
---
+++ push-logos.js
@@ -0,0 +1,57 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+const SERVER_URL = "http://localhost:3007/organizations";
+const TOKEN = "";
+const LOGO_DIRECTORY = "./export";
+
+const logoIds = require("./logo_ids.json");
+
+fs.readdir(LOGO_DIRECTORY, async (err, files) => {
+  if (err) {
+    console.error("Error reading directory:", err);
+    return;
+  }
+
+  for (const logoFilename of files) {
+    const logoFilePath = path.join(LOGO_DIRECTORY, logoFilename);
+
+    if (fs.statSync(logoFilePath).isFile()) {
+      const logoName = logoFilename.split(".png")[0];
+      const logoId = logoIds.find(
+        ({ name }) => name.replace(/\./g, "") === logoName
+      )?.id;
+
+      if (logoId) {
+        const base64EncodedLogo = `data:image/png;base64,${fs.readFileSync(
+          logoFilePath,
+          "base64"
+        )}`;
+
+        try {
+          const response = await axios.patch(
+            `${SERVER_URL}/${logoId}`,
+            {
+              logo: base64EncodedLogo,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${TOKEN}`,
+              },
+            }
+          );
+
+          console.log(`Logo ${logoName} updated. Response:`, response.data);
+        } catch (error) {
+          console.error(
+            `Error updating logo ${logoName}:`,
+            error.response.data
+          );
+        }
+      } else {
+        console.warn(`No logo ID found for ${logoName}. Skipping.`);
+      }
+    }
+  }
+});
